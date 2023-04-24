31264e0eed01d4d95cd280db0a6cb569d63c56e2
Lambert-Rao <lambert-teddy-smith@outlook.com>
2023-04-24 10:08:31 +0800

    !old version backup!

--- backend_srcs/base/ss_thread_pool.cpp
+++ backend_srcs/base/ss_thread_pool.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "ss_thread_pool.h"
+ThreadPool::ThreadPool() {
+    thread_num_ = 1;
+    terminate_ = false;
+}
+
+ThreadPool::~ThreadPool() {
+    Stop();
+}
+
+bool ThreadPool::Init(size_t worker_num) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    if (!threads_.empty()) {
+        return false;
+    }
+    thread_num_ = worker_num;
+    return true;
+}
+
+void ThreadPool::Stop() {
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        terminate_ = true;
+        work_cond_.notify_all();
+    }
+    for (auto &thread : threads_) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+        delete &thread;
+    }
+    std::unique_lock<std::mutex> lock(mutex_);
+    threads_.clear();
+}
--- backend_srcs/base/ss_thread_pool.h
+++ backend_srcs/base/ss_thread_pool.h
@@ -11,7 +11,17 @@
 #include <utility>
 #include <queue>
 
-class TreadPool {
+/**
+ * to use the thread pool, you should call Init(n) first
+ * then call Start() to start the thread pool
+ * after that, you can call Exec() to execute a task
+ * if you need to wait for all done, call WaitAllDone()
+ * At last, call Stop() to stop the thread pool
+ */
+
+
+
+class ThreadPool {
  protected:
   struct Task {
     Task(std::chrono::milliseconds interval, std::function<void()> task)
@@ -26,19 +36,20 @@ class TreadPool {
     std::chrono::steady_clock::time_point time_point_;
     std::chrono::milliseconds interval_;
   };
-  using TaskPtr = std::shared_ptr<Task>;
+  using TaskPtr = std::unique_ptr<Task>;
 
   std::queue<TaskPtr> tasks_;
   std::vector<std::thread> threads_;
   std::mutex mutex_;
-  std::condition_variable cond_var_;
+  std::condition_variable work_cond_;
+  //cond_var used when TODO ?
   size_t thread_num_;
-  bool terminate_;
+  std::atomic<bool> terminate_;
   std::atomic<int> atomic_{0};
 
  public:
-  TreadPool();
-  virtual ~TreadPool();
+  ThreadPool();
+  virtual ~ThreadPool();
   bool Init(size_t worker_num);
   //num of all threads
   size_t GetThreadNum();
@@ -46,24 +57,30 @@ class TreadPool {
   size_t GetJobNum();
   void Stop();
   bool Start();
+
+
   template<typename Func, typename... Args>
   auto Exec(Func &&f, Args &&...args) -> std::future<decltype(f(args...))> {
     return Exec(std::chrono::milliseconds(0), std::forward<Func>(f),
                 std::forward<Args>(args)...);
   }
+  /*
+   * Exec() do not do the work right now
+   */
   template<typename Func, typename... Args>
   auto Exec(std::chrono::milliseconds timeout, Func &&f, Args &&...args)
   -> std::future<decltype(f(args...))> {
     using RetType = decltype(f(args...));
-    auto task = std::make_shared<std::packaged_task<RetType()>>(
+    auto task = std::make_unique<std::packaged_task<RetType()>>(
         std::bind(std::forward<Func>(f), std::forward<Args>(args)...));
-    TaskPtr task_ptr = std::make_shared<Task>(timeout, [task]() { (*task)(); });
+    TaskPtr task_ptr = std::make_unique<Task>(timeout, [task]() { (*task)(); });
 
-    std::unique_lock<std::mutex> lock(mutex_);
+    std::lock_guard<std::mutex> lock(mutex_);
     tasks_.push(task_ptr);
-    cond_var_.notify_one();//wake up one thread to do the new task
+    work_cond_.notify_one();//wake up one thread to do the new task
     return task->get_future();
   }
+
 
   bool WaitAllDone(std::chrono::milliseconds timeout = std::chrono::milliseconds(-1));
 
@@ -72,5 +89,5 @@ class TreadPool {
  protected:
   bool GetTask(TaskPtr &task);//get a task from task queue
   void Run();
-  bool IsTterminate() const { return terminate_; }
+  bool IsTerminate() const { return terminate_.load(); }
 };
